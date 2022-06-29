2bd59ffca2f13b9bb1760f968e79544d73a5dfa9
Burak Kosova <burak.kosova25@gmail.com>
2022-06-29 19:40:05 +0300

    jwt için authorization klasörüne yardımcı classlar eklendi

---
+++ Business/Helpers/Authorization/AllowAnonymousAttribute.cs
@@ -0,0 +1,5 @@
+﻿namespace Business.Helpers.Authorization;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AllowAnonymousAttribute : Attribute
+{ }
---
+++ Business/Helpers/Authorization/AuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿namespace Business.Helpers.Authorization;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Model;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    private readonly IList<RoleEnum> _roles;
+
+    public AuthorizeAttribute(params RoleEnum[] roles)
+    {
+        _roles = roles ?? new RoleEnum[] { };
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // skip authorization if action is decorated with [AllowAnonymous] attribute
+        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+        if (allowAnonymous)
+            return;
+
+        // authorization
+        var account = (Account)context.HttpContext.Items["Account"];
+
+        var hasAuthority = false;
+        foreach(Role r in account.Roles)
+        {
+            if(_roles.Contains(r.RoleValue)) hasAuthority = true;
+        }
+
+        if (account == null || (_roles.Any() && !hasAuthority))
+        {
+            // not logged in or role not authorized
+            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+    }
+}
