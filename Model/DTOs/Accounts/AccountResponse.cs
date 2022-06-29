f932c5c86c72a74f0de05d68004b99db8da44e49
Burak Kosova <burak.kosova25@gmail.com>
2022-06-29 19:39:12 +0300

    Account modeli ve dto ları oluşturuldu

---
+++ Model/DTOs/Accounts/AccountResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Model.DTOs.Accounts;
+
+public class AccountResponse
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public List<Role> Roles { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public string? GroupName { get; set; }
+}
---
+++ Model/DTOs/Accounts/AuthenticateResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Model.DTOs.Accounts;
+
+public class AuthenticateResponse
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public List<Role> Roles { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public int? GroupId { get; set; }
+    public string JwtToken { get; set; }
+}
